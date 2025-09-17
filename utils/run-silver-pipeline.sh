d42b4ecddce5c91b26f5a4feaa22c2673a4cec09
bitterfq <amndp64@gmail.com>
2025-09-17 09:20:19 -0700

    Refactor SPARK_PACKAGES variable for improved readability; update default catalog configuration to use glue_catalog.

--- utils/run-silver-pipeline.sh
+++ utils/run-silver-pipeline.sh
@@ -3,7 +3,13 @@
 set -e
 
 # Configuration
-SPARK_PACKAGES="org.apache.iceberg:iceberg-spark-runtime-3.5_2.12:1.5.2,org.apache.hadoop:hadoop-aws:3.3.4,com.amazonaws:aws-java-sdk-bundle:1.12.262"
+SPARK_PACKAGES="\
+org.apache.iceberg:iceberg-spark-runtime-3.5_2.12:1.5.2,\
+org.apache.iceberg:iceberg-aws:1.5.2,\
+software.amazon.awssdk:bundle:2.20.160,\
+org.apache.hadoop:hadoop-aws:3.3.4,\
+com.amazonaws:aws-java-sdk-bundle:1.12.262"
+
 ICEBERG_WAREHOUSE=${ICEBERG_WAREHOUSE:-"s3a://cdf-silver/warehouse/"}
 RUN_DATE=${1:-$(date +%Y-%m-%d)}
 
@@ -16,7 +22,7 @@ rm -rf ./spark-warehouse
 spark-submit \
   --packages $SPARK_PACKAGES \
   --conf spark.sql.extensions=org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions \
-  --conf spark.sql.defaultCatalog=cdf \
+  --conf spark.sql.defaultCatalog=glue_catalog \
   --conf spark.sql.warehouse.dir=/tmp/spark-warehouse \
   --conf spark.serializer=org.apache.spark.serializer.KryoSerializer \
   --conf spark.sql.adaptive.enabled=true \
