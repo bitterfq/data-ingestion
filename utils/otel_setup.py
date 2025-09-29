92dca8ecdc17f8b54a8fbde224c1c7fd29aff925
bitterfq <amndp64@gmail.com>
2025-09-29 09:13:04 -0700

    Jaeger implementation added

--- utils/otel_setup.py
+++ utils/otel_setup.py
@@ -1,10 +1,16 @@
 from opentelemetry import trace
+from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 
 def init_tracer(service_name: str):
-    provider = TracerProvider()
-    processor = BatchSpanProcessor(ConsoleSpanExporter())
-    provider.add_span_processor(processor)
+    resource = Resource.create({"service.name": service_name})
+    provider = TracerProvider(resource=resource)
+
+    # send spans to Jaeger over OTLP
+    otlp_exporter = OTLPSpanExporter(endpoint="http://localhost:4318/v1/traces")
+    provider.add_span_processor(BatchSpanProcessor(otlp_exporter))
+
     trace.set_tracer_provider(provider)
     return trace.get_tracer(service_name)
